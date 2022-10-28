c729c93fb5a0069ece489949f0edf9671c79a928
EvgeniiDu <e.dubiniak@gmail.com>
2022-10-28 16:32:03 +0300

    add carousel on home page

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    $(".owl-carousel-hot-products").owlCarousel({
+        loop: true,
+        nav: true,
+        items: 4,
+        autoplay: false
+    });
+    $(".owl-carousel-top-products").owlCarousel({
+        loop: true,
+        nav: true,
+        items: 4,
+        autoplay: false
+    });
 });
